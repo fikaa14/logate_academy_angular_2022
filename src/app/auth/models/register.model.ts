7fa54d564d5181ce725fa868500032190c1dc089
filip <fikaaaa14@gmail.com>
2022-03-01 19:26:07 +0100

    Register Model modifikovan

--- src/app/auth/models/register.model.ts
+++ src/app/auth/models/register.model.ts
@@ -1,11 +1,7 @@
-import { Role } from "src/app/admin/roles/model/role.model";
-
 export interface Register{
     firstName: string,
     lastName: string,
     username: string,
     password: string,
-    roles: Role[
-        
-    ] 
+    roles: string[] 
 }
