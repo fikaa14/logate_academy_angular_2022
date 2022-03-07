57a714133da139a109b4cf4d0e15e10a4b841da1
filip <fikaaaa14@gmail.com>
2022-03-07 15:17:54 +0100

    pageable implemented

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,32 +1,63 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnDestroy, OnInit } from "@angular/core";
 import { FormControl, FormGroup } from "@angular/forms";
-import { debounceTime, switchMap } from "rxjs";
+import { debounceTime, Observable, pipe, Subject, switchMap, takeUntil } from "rxjs";
 import { ProductService } from "./product.service";
 
 @Component({
     selector: 'app-product',
     templateUrl: './product.component.html'
 })
-export class ProductComponent implements OnInit {
+export class ProductComponent implements OnInit, OnDestroy {
 
-    products: any[] = [];
-    productSearchFrom!: FormGroup;
+    products?: any[];
+    productSearchForm!: FormGroup;
+    page: number = 0;
+    size: number = 4;
+    memoryLeakSubject = new Subject();
+    enableButton: boolean = false;
+
 
     constructor(
         private productService: ProductService
     ) { }
 
+    ngOnDestroy(): void {
+        this.products = [];
+        this.memoryLeakSubject.complete();
+    }
+
+    loadMore() {
+        this.page++;
+        const searchTerm:string = this.productSearchForm.get('searchTerm')?.value;
+        const products$: Observable<any> = this.productService.searchByTermPageable(searchTerm, this.page, this.size);
+        products$.pipe(
+            takeUntil(this.memoryLeakSubject)
+        ).subscribe(data => {
+            this.products = this.products?.concat(data);
+            if(data.length < this.size) {
+                this.enableButton = true;
+            } else {
+                this.enableButton = false;
+            }
+            console.log("Products length: ", this.products?.length);
+        });
+    }
+
     ngOnInit(): void {
         this.initializeForm();
 
-        this.productSearchFrom.get('searchTerm')?.valueChanges
+        this.productSearchForm.get('searchTerm')?.valueChanges
             .pipe(
                 debounceTime(500),
                 switchMap(value => {
-                    return this.productService.searchByTerm(value);
+                    this.page = 0;
+                    return this.productService.searchByTermPageable(value, this.page, this.size);  
                 })
             ).subscribe(data => {
                 this.products = data;
+                if(this.products?.length === 4) {
+                    this.enableButton = false;
+                }
             }, error => {
                 console.log(error);
                 // this.products = [];
@@ -34,7 +65,7 @@ export class ProductComponent implements OnInit {
     }
 
     private initializeForm(): void {
-        this.productSearchFrom = new FormGroup({
+        this.productSearchForm = new FormGroup({
             searchTerm: new FormControl(null)
         });
     }
--- src/app/product/product.service.ts
+++ src/app/product/product.service.ts
@@ -1,15 +1,25 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { Observable } from "rxjs";
+import { map, Observable, tap } from "rxjs";
 import { environment } from "src/environments/environment";
 
 @Injectable({ providedIn: 'root'})
 export class ProductService {
 
     constructor(private httpClient: HttpClient) { }
+    oldTerm: string = '';
 
     searchByTerm(searchTerm: string): Observable<any> {
         const url = `${environment.apiUrl}product/search/by-name-or-desc?term=${searchTerm}`;
+        return this.httpClient.get(url);
+    }
+
+    searchByTermPageable(searchTerm: string, page: number, size: number): Observable<any> {
+        if(searchTerm != this.oldTerm) {
+            page = 0;
+            this.oldTerm = searchTerm;
+        }
+        const url = `${environment.apiUrl}product/search/by-name-or-desc?term=${searchTerm}&page=${page}&size=${size}`;
         return this.httpClient.get(url);
     }
 
